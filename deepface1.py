9eb55c12b549bb4f60ebc444bd91624f517cde01
Kush Bhardwaj <bhardwajkush01@gmail.com>
2022-08-06 21:34:13 -0400

    v1

---
+++ deepface1.py
@@ -0,0 +1,23 @@
+# emotion_detection.py
+from cv2 import imshow
+import keras
+import tensorflow
+import cv2
+from deepface import DeepFace
+import numpy as np  # this will be used later in the process
+
+# put the image where this file is located and put its name here
+imgpath = 'data/frame37.jpg'
+image = cv2.imread(imgpath)
+
+obj = DeepFace.analyze(img_path="data/frame37.jpg",
+                       actions=['age', 'gender', 'race', 'emotion'])
+print("printing the result")
+print(obj)
+imshow("image", image)
+# print(tensorflow.__version__)
+# print(keras.__version__)
+
+# here the first parameter is the image we want to analyze #the second one there is the action
+# analyze = DeepFace.analyze(image, actions=['emotions'])
+# print(analyze)
